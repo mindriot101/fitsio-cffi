40d1265c7cf54611f522b0f6b55d65efff54f7e1
Simon Walker <s.r.walker101@googlemail.com>
2015-05-31 22:05:49 +0100

    Add version and url

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
         name='cffitsio',
+        version='0.0.1',
+        url='https://github.com/mindriot101/fitsio-cffi',
         packages=find_packages('src'),
         package_dir={'': 'src'},
         setup_requires=['cffi>=1.0.0'],
