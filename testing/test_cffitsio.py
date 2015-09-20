b94edbbb717313cc831fa97d3ccf9ab715ff3ade
Simon Walker <s.r.walker101@googlemail.com>
2015-09-20 21:30:37 +0100

    Add test for opening file

--- testing/test_cffitsio.py
+++ testing/test_cffitsio.py
@@ -1,7 +1,21 @@
-from cffitsio import FitsFile
+import pytest
+import cffitsio
 import os
+
+
+@pytest.fixture
+def test_dir():
+    return os.path.join(
+        os.path.dirname(__file__),
+        'data')
 
 def test_create_file(tmpdir):
     filename = str(tmpdir.join('test.fits'))
-    f = FitsFile.create(filename)
+    f = cffitsio.FitsFile.create(filename)
     assert os.path.isfile(filename)
+
+
+def test_open_file(test_dir):
+    filename = os.path.join(test_dir, 'all.fits')
+    with cffitsio.open_fits(filename) as infile:
+        assert isinstance(infile, cffitsio.FitsFile)
