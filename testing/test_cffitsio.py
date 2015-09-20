71cd41215f7fabcecabaf910cb81d358e6c8bc62
Simon Walker <s.r.walker101@googlemail.com>
2015-09-20 21:34:18 +0100

    Refactor test_hdulist yield fixture

--- testing/test_cffitsio.py
+++ testing/test_cffitsio.py
@@ -5,14 +5,18 @@ import os
 
 @pytest.fixture
 def test_dir():
-    return os.path.join(
-        os.path.dirname(__file__),
-        'data')
+    return os.path.join(os.path.dirname(__file__), 'data')
 
 
 @pytest.fixture
 def test_file(test_dir):
     return os.path.join(test_dir, 'all.fits')
+
+
+@pytest.yield_fixture
+def test_hdulist(test_file):
+    with cffitsio.open_fits(test_file) as infile:
+        yield infile
 
 
 def test_create_file(tmpdir):
@@ -21,11 +25,9 @@ def test_create_file(tmpdir):
     assert os.path.isfile(filename)
 
 
-def test_open_file(test_file):
-    with cffitsio.open_fits(test_file) as infile:
-        assert isinstance(infile, cffitsio.FitsFile)
+def test_open_file(test_hdulist):
+    assert isinstance(test_hdulist, cffitsio.FitsFile)
 
 
-def test_read_hdus(test_file):
-    with cffitsio.open_fits(test_file) as infile:
-        assert len(infile) == 3
+def test_read_hdus(test_hdulist):
+    assert len(test_hdulist) == 3
