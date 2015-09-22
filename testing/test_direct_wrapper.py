0a0b87d584bd731c1db65e32a7e438b0f9aea1a9
Simon Walker <s.r.walker101@googlemail.com>
2015-09-22 15:19:23 +0100

    Add test for open file

--- testing/test_direct_wrapper.py
+++ testing/test_direct_wrapper.py
@@ -10,3 +10,10 @@ def test_create_file(tmpdir):
     lib.fits_create_file(f, filename, status)
     assert status[0] == 0
     assert os.path.isfile(filename)
+
+
+def test_open_file(test_file):
+    f = ffi.new('fitsfile **')
+    status = ffi.new('int *')
+    lib.fits_open_file(f, test_file, 0, status)
+    assert status[0] == 0
